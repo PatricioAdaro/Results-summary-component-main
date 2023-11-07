c8b942437012d4d8c8d4875d86abf3b11e652746
“Patricio <adaropato2@gmail.com>
2023-11-07 12:25:08 -0300

    results-summary-2

--- script.js
+++ script.js
@@ -2,13 +2,12 @@
 fetch ("./data.json")
     .then(res => res.json ())
     .then(data =>{
-       console.log(data)
        const background = ["react", "memo", "ver", "vis"];
        const color = ["reaction", "memory", "verbal", "visual"];
        data.forEach((element, index) => {
         let container=document.querySelector(".container-category")
         container.innerHTML += `
-                                <div id="all-category" class="category ${background[index]}">
+                                <div class="category ${background[index]}">
                                 <div class="${color[index]}">
                                     <img src= "${element.icon}" alt="reaction">
                                     <p>${element.category}</p>
